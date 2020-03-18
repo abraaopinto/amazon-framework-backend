3ad340b2f0cf7c3ee3bc9f09d7edf23988978cb9
abraaopinto <pxzabraao13@gmail.com>
2020-03-18 20:35:50 -0300

    atualizando versão para teste de log

--- src/config/winston.js
+++ src/config/winston.js
@@ -1,50 +1,52 @@
-var appRoot = require('app-root-path');
-var winston = require('winston');
+const appRoot = require('app-root-path');
+const winston = require('winston');
 
+const logName = process.env.npm_package_name || 'api';
+const logErrorName = logName + 'Error'
 // define the custom settings for each transport (file, console)
 const options = {
-    file: {
-      level: 'debug',
-      filename: `${appRoot}/logs/${process.env.npm_package_name || 'app'}.log`,
-      handleExceptions: false,
-      json: true,
-      maxsize: 5242880, // 5MB
-      maxFiles: 5,
-      colorize: false,
-    },
-    console: {
-      level: 'info',
-      handleExceptions: false,
-      json: false,
-      colorize: false,
-    },
-    fileError:{
-      level: 'error',
-      filename: `${appRoot}/logs/${process.env.npm_package_name+'Error' || 'appErro'}.log`,
-      handleExceptions: true,
-      json: true,
-      maxsize: 5242880, // 5MB
-      maxFiles: 5,
-      colorize: false,
-      humanReadableUnhandledException: true
-    }
-  };
+  file: {
+    level: 'debug',
+    filename: `${appRoot}/logs/${logName}.log`,
+    handleExceptions: false,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: false,
+  },
+  console: {
+    level: 'info',
+    handleExceptions: false,
+    json: false,
+    colorize: false,
+  },
+  fileError: {
+    level: 'error',
+    filename: `${appRoot}/logs/${logErrorName}.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: false,
+    humanReadableUnhandledException: true
+  }
+};
 
-  // instantiate a new Winston Logger with the settings defined above
-  var logger = winston.createLogger({
-    transports: [
-      new (winston.transports.Console)(options.console),
-      new (winston.transports.File)(options.file),
-      new (winston.transports.File)(options.fileError)
-    ],
-    exitOnError: false, // do not exit on handled exceptions
-  });
+// instantiate a new Winston Logger with the settings defined above
+var logger = winston.createLogger({
+  transports: [
+    new (winston.transports.Console)(options.console),
+    new (winston.transports.File)(options.file),
+    new (winston.transports.File)(options.fileError)
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+});
 
-  logger.stream = {
-    write: function(message, encoding) {
-      // use the 'info' log level so the output will be picked up by both transports (file and console)
-      logger.info(message);
-    },
-  };
+logger.stream = {
+  write: function (message, encoding) {
+    // use the 'info' log level so the output will be picked up by both transports (file and console)
+    //logger.info(message);
+  },
+};
 
-  module.exports = logger;
+module.exports = logger;
