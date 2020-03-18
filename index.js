2f2c384e660e3b5a054e2d64d3085427935602c4
abraaopinto <pxzabraao13@gmail.com>
2020-03-18 17:00:51 -0300

    correção do nome da propriedade

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./src/config/express')();
-const port = app.get('server.port');
+const port = app.get('port');
 
 app.listen(port, () => console.log(`EXECUTANDO APLICAÇÃO NA PORTA ${port}`));
 
-//module.exports = app;
+module.exports = app;
